ec5da2a9aaa494d0d76b77b822faf7d54354e77b
Adam Louis <adamlouis@gmail.com>
2015-03-03 17:53:34 -0800

    seed records in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env.development?
+  5.times do
+    Category.create!(name: Faker::Commerce.department)
+  end
+  categories = Category.all
+
+  10.times do
+    User.create!(email: Faker::Internet.email,
+                 password: Faker::Internet.password,
+                 name: Faker::Name.name)
+  end
+  users = User.all
+
+  users.take(5).each do |user|
+    Seller.create!(user: user,
+                   confirmed: [true, false].sample)
+  end
+  sellers = Seller.all
+  confirmed_sellers = Seller.where(confirmed: true)
+  confirmed_seller_users = confirmed_sellers.map{ |seller| seller.user }
+
+  20.times do
+    Listing.create!(name: Faker::Commerce.product_name,
+                    description: Faker::Lorem.paragraph,
+                    price: Faker::Commerce.price,
+                    user: confirmed_seller_users.sample,
+                    category: categories.sample,
+                    image: Rails.root.join("app/assets/images/favicon.png").open)
+  end
+  listings = Listing.all
+
+  20.times do
+    Order.create!(address: Faker::Address.street_address,
+                  city: Faker::Address.city,
+                  state: Faker::Address.state,
+                  listing: listings.sample,
+                  buyer: users.sample,
+                  seller: confirmed_seller_users.sample)
+  end
+  orders = Order.all
+
+  User.create!(email: 'admin@example.com',
+               password: '12345678',
+               name: 'Admin',
+               role: 'admin')
+  users = User.all
+
+  [Category, User, Seller, Listing, Order].each do |model|
+    puts "#{model}: #{model.count}"
+  end
+end
