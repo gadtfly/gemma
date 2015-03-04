6e5f8f07d6dbc6123028c5476bc164af5529913b
Adam Louis <adamlouis@gmail.com>
2015-03-03 17:52:50 -0800

    fix shopping cart session lookup to allow for inconsistent sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) << :name
 
   end
-  private
+
+private
+
   def extract_shopping_cart
-    shopping_cart_id = session[:shopping_cart_id]
-    @shopping_cart = session[:shopping_cart_id] ? ShoppingCart.find(shopping_cart_id) : ShoppingCart.create
+    @shopping_cart = ShoppingCart.find_by(id: session[:shopping_cart_id]) || ShoppingCart.create
     session[:shopping_cart_id] = @shopping_cart.id
   end
 end
